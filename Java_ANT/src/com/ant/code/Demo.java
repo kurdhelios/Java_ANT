1c6ec638ad9e71ec690cb82a3185ce3ac2e8bae2
et32003 <et32003@SE-R9EV5AG.corp.corpcommon.com>
2014-02-20 14:03:58 +0100

    New display methos included for testing.

--- Java_ANT/src/com/ant/code/Demo.java
+++ Java_ANT/src/com/ant/code/Demo.java
@@ -3,8 +3,18 @@ package com.ant.code;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Executing Demo....");
+		display();
 
+	}
+
+	private static void display() {
+		int start = 0;
+		int destination = 100;
+		System.out.println("Executing Demo....");
+		System.out.println("\n" + start + ":" + destination);
+		for (start = 1; start < destination; start++) {
+			System.out.println("\n" + start + ":" + destination);
+		}
 	}
 
 }
